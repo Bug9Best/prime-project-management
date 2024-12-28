f819631be238b351ca63f442c677b4e62d0c415f
Natthaphon Ditthaviboon <bug9best@gmail.com>
2024-12-28 18:34:35 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { TopBar } from '../components/layout/topbar/top-bar.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: TopBar
     }
 ];

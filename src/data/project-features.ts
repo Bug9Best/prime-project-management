13698398ccdd6e916c5051d0a013dfeb4952fb3a
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-04 19:35:21 +0700

    add list data

---
+++ src/data/project-features.ts
@@ -0,0 +1,33 @@
+export var listProjectFeatures = [
+  {
+    title: 'FEATURE_SCRUM',
+    description: 'FEATURE_SCRUM_DESCRIPTION',
+    icon: 'pi-spinner'
+  },
+  {
+    title: 'FEATURE_WATER_FALL',
+    description: 'FEATURE_WATER_FALL_DESCRIPTION',
+    icon: 'pi-check-circle'
+  },
+  {
+    title: 'FEATURE_KANBAN',
+    description: 'FEATURE_KANBAN_DESCRIPTION',
+    icon: 'pi-clipboard'
+  },
+  {
+    title: 'FEATURE_MEMBER',
+    description: 'FEATURE_MEMBER_DESCRIPTION',
+    icon: 'pi-users'
+  },
+  {
+    title: 'FEATURE_RESOURCE',
+    description: 'FEATURE_RESOURCE_DESCRIPTION',
+    icon: 'pi-file'
+  },
+  {
+    title: 'FEATURE_NOTIFY',
+    description: 'FEATURE_NOTIFY_DESCRIPTION',
+    icon: 'pi-send'
+  },
+
+]
---
+++ src/data/project-ontributes.ts
@@ -0,0 +1,22 @@
+export var listProjectContributes = [
+  {
+    full_name: 'ADVISER_PORNSUREE',
+    position: 'ADVISER_PORNSUREE_POSITION',
+    imageURL: '/image/Pornsuree.jpg'
+  },
+  {
+    full_name: 'ADVISER_TARAVICHET',
+    position: 'ADVISER_TARAVICHET_POSITION',
+    imageURL: '/image/Taravichet.jpg'
+  },
+  {
+    full_name: 'DEVELOPER_NATTHAPHON',
+    position: 'DEVELOPER_NATTHAPHON_POSITION',
+    imageURL: '/image/Natthaphon.jpg'
+  },
+  {
+    full_name: 'DEVELOPER_PHURI',
+    position: 'DEVELOPER_PHURI_POSITION',
+    imageURL: '/image/Phuri.jpg'
+  }
+]

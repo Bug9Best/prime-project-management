43ffb40c9c48154b97d61b924841c5a039deda50
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-04 19:35:14 +0700

    remove ng chaos and set default language

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,8 +11,6 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import MyPreset from '../preset/preset';
-import { ChaosModule } from 'ngchaos/core';
-import { ChaosAuthModule } from 'ngchaos/auth';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, '/i18n/', '.json');
@@ -28,24 +26,13 @@ export const appConfig: ApplicationConfig = {
       BrowserModule,
       BrowserAnimationsModule,
       TranslateModule.forRoot({
+        defaultLanguage: 'en',
         loader: {
           provide: TranslateLoader,
           useFactory: HttpLoaderFactory,
           deps: [HttpClient],
         },
       }),
-      ChaosModule.forRoot({
-        appId: 'prime',
-        clientId: 'prime',
-        title: 'prime',
-        subTitle: 'prime project management',
-        icon: 'logo.png',
-        endpoint: 'http://localhost:8080',
-        databaseUrl: 'http://localhost:8080/api',
-        authPath: '/auth',
-        home: '/home'
-      }),
-      ChaosAuthModule.forRoot(),
     ]),
     provideAnimationsAsync(),
     providePrimeNG({

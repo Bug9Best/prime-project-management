147e6f9d097373c9bcea76f15a6964244f2419dd
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-05 11:29:26 +0700

    update preset

--- src/preset/preset.ts
+++ src/preset/preset.ts
@@ -1,6 +1,5 @@
 import { definePreset } from '@primeng/themes';
 import Aura from '@primeng/themes/aura';
-import Lara from '@primeng/themes/lara';
 
 const MyPreset = definePreset(Aura, {
     semantic: {
@@ -18,42 +17,6 @@ const MyPreset = definePreset(Aura, {
             950: '{blue.950}'
         },
     },
-    colorScheme: {
-        light: {
-            primary: {
-                color: '{blue.950}',
-                inverseColor: '#ffffff',
-                hoverColor: '{blue.900}',
-                activeColor: '{blue.800}'
-            },
-            highlight: {
-                background: '{blue.950}',
-                focusBackground: '{blue.700}',
-                color: '#ffffff',
-                focusColor: '#ffffff'
-            },
-            formField: {
-                hoverBorderColor: '{primary.color}'
-            }
-        },
-        dark: {
-            primary: {
-                color: '{zinc.50}',
-                inverseColor: '{zinc.950}',
-                hoverColor: '{zinc.100}',
-                activeColor: '{zinc.200}'
-            },
-            highlight: {
-                background: 'rgba(250, 250, 250, .16)',
-                focusBackground: 'rgba(250, 250, 250, .24)',
-                color: 'rgba(255,255,255,.87)',
-                focusColor: 'rgba(255,255,255,.87)'
-            },
-            formField: {
-                hoverBorderColor: '{primary.color}'
-            }
-        }
-    }
 });
 
 export default MyPreset;

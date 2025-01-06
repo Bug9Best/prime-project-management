4fc751880fa6211c5d379eaf478676dba511fcfa
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-06 10:25:09 +0700

    add environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://www.primefaces.org/data/customers'
+    apiUrl: 'https://www.primefaces.org/data/customers',
+    authenUrl: 'http://localhost:8000/login',
+    signupUrl: 'http://localhost:8000/register'
 };

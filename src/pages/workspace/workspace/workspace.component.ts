f6ca229ab4f28952f0232f2ceafb7d35cbdd59c7
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-27 06:52:21 +0700

    done main workspace component

--- src/pages/workspace/workspace/workspace.component.ts
+++ src/pages/workspace/workspace/workspace.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { WorkspaceTopbar } from '../workspace-topbar/workspace-topbar.component';
+import { WorkspaceContentPage } from '../workspace-content/workspace-content.component';
+import { WorkspaceSidebar } from '../workspace-sidebar/workspace-sidebar.component';
 
 @Component({
   selector: 'workspace',
-  imports: [],
+  standalone: true,
+  imports: [
+    WorkspaceTopbar,
+    WorkspaceContentPage,
+    WorkspaceSidebar,
+  ],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
 })
 export class WorkspacePage {
 
+  currentTabIndex: number = 0;
 }

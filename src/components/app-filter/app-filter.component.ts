fff37f1497c0817d6d3616af9f60c37be62bfdfe
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-28 04:15:43 +0700

    done filter component

--- src/components/app-filter/app-filter.component.ts
+++ src/components/app-filter/app-filter.component.ts
@@ -1,18 +1,86 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { InputTextModule } from 'primeng/inputtext';
+import { MenuModule } from 'primeng/menu';
+import { TieredMenuModule } from 'primeng/tieredmenu';
 
 @Component({
   selector: 'app-filter',
   imports: [
+    CommonModule,
+    FormsModule,
     ButtonModule,
     InputTextModule,
-    DividerModule
+    DividerModule,
+    TranslateModule,
+    MenuModule,
+    TieredMenuModule,
   ],
   templateUrl: './app-filter.component.html',
   styleUrl: './app-filter.component.scss'
 })
 export class AppFilter {
 
+  searchValue: string = '';
+
+  groupValue: { label: string, value: string } = {
+    label: 'Group by none',
+    value: 'NONE'
+  };
+
+  sortValue: { label: string, value: string } = {
+    label: 'Sort by none',
+    value: 'NONE',
+  };
+
+  groupItems = [
+    {
+      label: 'APP_FILTER_GROUP_NONE',
+      value: 'NONE'
+    },
+    {
+      label: 'APP_FILTER_GROUP_TYPE',
+      value: 'TYPE'
+    },
+  ];
+
+  sortItems = [
+    {
+      label: 'APP_FILTER_SORT_NAME_ASC',
+      value: 'NAME_ASC',
+    },
+    {
+      label: 'APP_FILTER_SORT_NAME_DESC',
+      value: 'NAME_DESC',
+    },
+    {
+      label: 'APP_FILTER_SORT_TYPE_ASC',
+      value: 'TYPE_ASC',
+    },
+    {
+      label: 'APP_FILTER_SORT_TYPE_DESC',
+      value: 'TYPE_DESC',
+    },
+  ]
+
+  setGroupValueEvent = output<any>();
+  setGroupValue(value: { label: string, value: string }) {
+    this.groupValue = value;
+    this.setGroupValueEvent.emit(value);
+  }
+
+  setSortValueEvent = output<any>();
+  setSortValue(value: { label: string, value: string }) {
+    this.sortValue = value;
+    this.setSortValueEvent.emit(value);
+  }
+
+  onSearchEvent = output<string>();
+  onSearch() {
+    this.onSearchEvent.emit(this.searchValue);
+  }
 }

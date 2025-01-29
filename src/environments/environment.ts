698b6fcc1f31d2501b810d9057264fe5e62ba485
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-29 07:37:52 +0700

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://www.primefaces.org/data/customers',
+    apiUrl: "http://localhost:8000/api/",
     authenUrl: 'http://localhost:8000/login',
     signupUrl: 'http://localhost:8000/register'
 };

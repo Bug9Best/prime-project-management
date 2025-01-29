2af54021776e958fbb81c719cff88ec1142dae25
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-29 09:18:21 +0700

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,52 +15,8 @@ export const routes: Routes = [
     path: 'workspace',
     loadChildren: () => import('../pages/workspace/workspace.module').then(m => m.WorkspaceModule)
   },
-  // {
-  //   path: 'project/:id',
-  //   component: PhantomPage,
-  //   children: [
-  //       {
-  //           path: '',
-  //           redirectTo: 'dashboard',
-  //           pathMatch: 'prefix'
-  //       },
-  //       {
-            
-  //           path: 'dashboard',
-  //           loadChildren: () => import('./features/project/project-dashboard/project-dashboard.module').then(m => m.ProjectDashboardModule)
-  //       },
-  //       {
-  //           path: 'sprint',
-  //           loadChildren: () => import('./features/project/project-sprint/project-sprint.module').then(m => m.ProjectSprintModule)
-  //       },
-  //       {
-  //           path: 'backlog',
-  //           loadChildren: () => import('./features/project/project-backlog/project-backlog.module').then(m => m.ProjectBacklogModule)
-  //       },
-  //       {
-  //           path: 'task',
-  //           loadChildren: () => import('./features/project/project-issue/project-issue.module').then(m => m.ProjectIssueModule)
-  //       },
-  //       {
-  //           path: 'board',
-  //           loadChildren: () => import('./features/project/project-board/project-board.module').then(m => m.ProjectBoardModule)
-  //       },
-  //       {
-  //           path: 'gantt',
-  //           loadChildren: () => import('./features/project/project-gantt/project-gantt.module').then(m => m.ProjectGanttModule)
-  //       },
-  //       {
-  //           path: 'resource',
-  //           loadChildren: () => import('./features/project/project-resource/project-resource.module').then(m => m.ProjectResourceModule)
-  //       },
-  //       {
-  //           path: 'member',
-  //           loadChildren: () => import('./features/project/project-member/project-member.module').then(m => m.ProjectMemberModule)
-  //       },
-  //       {
-  //           path: 'setting',
-  //           loadChildren: () => import('./features/project/project-setting/project-setting.module').then(m => m.ProjectSettingModule)
-  //       },
-  //   ]
-  // },
+  {
+    path: 'project/:id',
+    loadChildren: () => import('../pages/project/project/project.module').then(m => m.ProjectModule)
+  },
 ];

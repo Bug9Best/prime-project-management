2f9e1bbd4e30193eb403d0ce02c73381aef70765
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-29 07:40:48 +0700

    done content project component

--- src/pages/workspace/workspace-content-project/workspace-content-project.component.ts
+++ src/pages/workspace/workspace-content-project/workspace-content-project.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { AppHeader } from '../../../components/app-header/app-header.component';
 import { CommonModule } from '@angular/common';
@@ -6,6 +6,10 @@ import { FormsModule } from '@angular/forms';
 import { AppFilter } from '../../../components/app-filter/app-filter.component';
 import { AccordionModule } from 'primeng/accordion';
 import { AppScrolling } from '../../../components/app-scrolling/app-scrolling.component';
+import { AppDialog } from '../../../components/app-dialog/app-dialog.component';
+import { FormProjectCreate } from '../form-project-create/form-project-create.component';
+import { MessageService } from 'primeng/api';
+import { ProjectService } from '../../../services/project/project.service';
 
 export type Mode = 'NONE' | 'GROUP';
 
@@ -15,8 +19,10 @@ export type Mode = 'NONE' | 'GROUP';
     CommonModule,
     FormsModule,
     AppHeader,
+    AppDialog,
     AppFilter,
     AppScrolling,
+    FormProjectCreate,
     ButtonModule,
     AccordionModule
   ],
@@ -38,100 +44,24 @@ export class WorkspaceContentProject {
   templistProject: any[] = [];
   grouplistProject: { [key: string]: any[] } = {};
 
+  constructor(
+    private messageService: MessageService,
+    private projectService: ProjectService
+  ) { }
+
   ngOnInit() {
-    this.listProject = [
-      {
-        id: 1,
-        project_name: "Y-find",
-        project_type: "Scrum Project",
-        created_at: "10/1/2024"
-      },
-      {
-        id: 2,
-        project_name: "Konklux",
-        project_type: "Scrum Project",
-        created_at: "1/29/2024"
-      },
-      {
-        id: 3,
-        project_name: "Opela",
-        project_type: "Kanban Project",
-        created_at: "11/2/2024"
-      },
-      {
-        id: 4,
-        project_name: "Bamity",
-        project_type: "Waterfall Project",
-        created_at: "9/29/2024"
-      },
-      {
-        id: 5,
-        project_name: "Alphazap",
-        project_type: "Scrum Project",
-        created_at: "6/1/2024"
-      },
-      {
-        id: 6,
-        project_name: "Tampflex",
-        project_type: "Waterfall Project",
-        created_at: "1/15/2025"
-      },
-      {
-        id: 7,
-        project_name: "Zamit",
-        project_type: "Kanban Project",
-        created_at: "9/5/2024"
-      },
-      {
-        id: 8,
-        project_name: "Tampflex",
-        project_type: "Kanban Project",
-        created_at: "7/24/2024"
-      },
-      {
-        id: 9,
-        project_name: "Zontrax",
-        project_type: "Scrum Project",
-        created_at: "11/29/2024"
-      },
-      {
-        id: 10,
-        project_name: "Y-Solowarm",
-        project_type: "Scrum Project",
-        created_at: "8/21/2024"
-      },
-      {
-        id: 11,
-        project_name: "Duobam",
-        project_type: "Waterfall Project",
-        created_at: "8/31/2024"
-      },
-      {
-        id: 12,
-        project_name: "Wrapsafe",
-        project_type: "Kanban Project",
-        created_at: "2/27/2024"
-      },
-      {
-        id: 13,
-        project_name: "Pannier",
-        project_type: "Waterfall Project",
-        created_at: "8/24/2024"
-      },
-      {
-        id: 14,
-        project_name: "Veribet",
-        project_type: "Kanban Project",
-        created_at: "10/19/2024"
-      },
-      {
-        id: 15,
-        project_name: "Lotstring",
-        project_type: "Waterfall Project",
-        created_at: "7/7/2024"
-      },
-    ]
-    this.templistProject = [...this.listProject];
+    this.getProjects();
+  }
+
+  getProjects() {
+    this.projectService
+      .getAll()
+      .subscribe({
+        next: (response: any) => {
+          this.listProject = response.data;
+          this.templistProject = [...this.listProject];
+        },
+      });
   }
 
   onSearch(event: any) {
@@ -219,5 +149,57 @@ export class WorkspaceContentProject {
       }
       this.listProject.sort(sortFunction);
     }
+  }
+
+  showMessage(severity: string, summary: string, detail: string): void {
+    this.messageService.add({
+      key: 'app',
+      severity: severity,
+      summary: summary,
+      detail: detail
+    });
+  }
+
+  resetForm() {
+    this.formProjectCreate.formGroup.reset();
+  }
+
+
+  @ViewChild(FormProjectCreate) formProjectCreate!: FormProjectCreate;
+  onValidateForm() {
+    let project_type = this.formProjectCreate.getProjectType();
+    this.formProjectCreate.formGroup
+      .patchValue({
+        project_type: project_type
+      });
+
+    if (this.formProjectCreate.formGroup.invalid) {
+      this.formProjectCreate.formGroup.markAllAsTouched();
+      console.log('Form invalid');
+      return;
+    }
+
+    let values = this.formProjectCreate.formGroup.value;
+    this.onCreateProject(values);
+  }
+
+  @ViewChild(AppDialog) appDialog!: AppDialog;
+  onCreateProject(param: any) {
+    console.log(param);
+    this.projectService
+      .createProject(param)
+      .subscribe({
+        next: (response) => {
+          this.showMessage('success', 'Success', 'Create project successfully');
+          this.appDialog.visible = false;
+          this.resetForm();
+          this.getProjects();
+        },
+        error: (error) => {
+          console.log(error);
+          this.showMessage('error', 'Error', 'Create project failed');
+        }
+      });
+
   }
 }

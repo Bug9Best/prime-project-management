d0ef522b01398e3d7468223df4163c4002bf6945
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-29 07:39:54 +0700

    done dialog component

---
+++ src/components/app-dialog/app-dialog.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  imports: [
+    CommonModule,
+    TranslateModule,
+    DialogModule,
+    ButtonModule
+  ],
+  templateUrl: './app-dialog.component.html',
+  styleUrl: './app-dialog.component.scss'
+})
+export class AppDialog {
+
+  showButton = input(true);
+  icon = input('pi pi-plus');
+  label = input('Open Dialog');
+  buttonStyleClass = input('border-round-lg');
+
+  visible: boolean = false;
+  header = input('Dialog Header');
+  modal = input(true);
+  draggable = input(false);
+  resizable = input(false);
+  closable = input(true);
+  width = input('50vw');
+  closeOnEscape = input(true);
+  dismissableMask = input(true);
+  showHeader = input(true);
+
+  onSubmitEvent = output<void>();
+  onSubmit() {
+    this.onSubmitEvent.emit();
+  }
+}

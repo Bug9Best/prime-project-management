75141fe613a696859fa41a1536df9854c4e9c748
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-29 07:37:37 +0700

    create new component for project create

---
+++ src/pages/workspace/form-project-create/form-project-create.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+import { AppForm, InputAppForm } from '../../../components/app-form/app-form.component';
+import { FormGroup } from '@angular/forms';
+import { FormGroupProjectCreate, listFormProjectCreate } from '../../../forms/project-create';
+
+@Component({
+  selector: 'form-project-create',
+  imports: [
+    AppForm,
+  ],
+  templateUrl: './form-project-create.component.html',
+  styleUrl: './form-project-create.component.scss'
+})
+export class FormProjectCreate {
+
+  listForm: InputAppForm[] = listFormProjectCreate;
+  formGroup: FormGroup = FormGroupProjectCreate;
+  toggleOptions: any[] = [
+    { value: 'PRIVATE', label: 'FORM_PROJECT_PRIVACY_TYPE_PRIVATE' },
+    { value: 'PUBLIC', label: 'FORM_PROJECT_PRIVACY_TYPE_PUBLIC' }
+  ]
+
+  @ViewChild(AppForm) AppForm!: AppForm;
+  getProjectType(): string {
+    return this.AppForm.selectedProjectType;
+  }
+}

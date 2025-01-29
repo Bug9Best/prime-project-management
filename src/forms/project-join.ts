e31ca999cf51593820fefdc48d3eae9c7ed5af22
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-29 08:38:02 +0700

    add form for project join

---
+++ src/forms/project-join.ts
@@ -0,0 +1,18 @@
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { InputAppForm } from "../components/app-form/app-form.component";
+
+export var FormGroupProjectJoin = new FormGroup({
+    project_code: new FormControl(null, [Validators.required]),
+});
+
+export var listFormProjectJoin: InputAppForm[] = [
+    {
+        type: "text",
+        label: "FORM_PROJECT_CODE",
+        field: "project_code",
+        required: true,
+        disabled: false,
+        colWidth: 12,
+        maxLength: 10,
+    },
+]

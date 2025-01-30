1bcf680914b1375af8b9394ac4ae70b6653edf23
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-01-30 08:02:52 +0700

    create new module and main feedback component

---
+++ src/pages/feedback/feedback/feedback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feedback-page',
+  imports: [],
+  templateUrl: './feedback.component.html',
+  styleUrl: './feedback.component.scss'
+})
+export class FeedbackPage {
+
+}

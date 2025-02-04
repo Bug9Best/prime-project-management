623835806b6da97ac4305f6f26947706ee16a014
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-05 04:05:37 +0700

    done main page for project setting

--- src/pages/project/project-setting/project-setting.component.ts
+++ src/pages/project/project-setting/project-setting.component.ts
@@ -1,12 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AppHeader } from '../../../components/app-header/app-header.component';
+import { TabViewModule } from 'primeng/tabview';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { FormsModule } from '@angular/forms';
+import { SettingGeneral } from './setting-general/setting-general.component';
+import { SettingCustomer } from './setting-customer/setting-customer.component';
+import { SettingNotification } from './setting-notification/setting-notification.component';
 
 @Component({
   selector: 'project-setting',
   imports: [
     CommonModule,
+    FormsModule,
+    TabViewModule,
     AppHeader,
+    SettingGeneral,
+    SettingCustomer,
+    SettingNotification
   ],
   templateUrl: './project-setting.component.html',
   styleUrl: './project-setting.component.scss'

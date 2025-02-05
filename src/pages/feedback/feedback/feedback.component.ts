069b21d7c0acc35fe87eb12f9670b24a11ecc26c
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-05 07:41:36 +0700

    done maon component of feedback

--- src/pages/feedback/feedback/feedback.component.ts
+++ src/pages/feedback/feedback/feedback.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FeedbackTopbar } from '../component/feedback-topbar/feedback-topbar.component';
+import { FeedbackSidebar } from '../component/feedback-sidebar/feedback-sidebar.component';
+import { FeedbackContentPage } from '../component/feedback-content/feedback-content.component';
 
 @Component({
   selector: 'feedback-page',
-  imports: [],
+  imports: [
+    FeedbackTopbar,
+    FeedbackSidebar,
+    FeedbackContentPage
+  ],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss'
 })
 export class FeedbackPage {
 
+  currentTabIndex: number = 0;
+
+  onChangeTabIndex(index: any): void {
+    this.currentTabIndex = index;
+  }
 }

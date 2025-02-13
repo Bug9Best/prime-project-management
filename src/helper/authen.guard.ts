5f4ac87e791988f2dca23e03c805c39cb941100b
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-13 16:46:55 +0700

    add authen guard

---
+++ src/helper/authen.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivateFn } from '@angular/router';
+import { environment } from '../environments/environment';
+import { AuthService } from '../services/auth/auth.service';
+import { inject } from '@angular/core';
+
+export const authenGuard: CanActivateFn = () => {
+  let authService = inject(AuthService);
+  let state = authService.getToken() ? true : false;
+  if (!state) {
+    window.location.href = environment.authenUrl;
+  }
+  return state
+};

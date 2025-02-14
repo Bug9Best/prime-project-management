e78c0a9078a2645c78ad1c2269da94940040011b
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-15 03:23:43 +0700

    add role condition

--- src/pages/workspace/component/workspace-sidebar/workspace-sidebar.component.ts
+++ src/pages/workspace/component/workspace-sidebar/workspace-sidebar.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, output } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { DividerModule } from 'primeng/divider';
+import { AuthService } from '../../../../services/auth/auth.service';
 
 @Component({
   selector: 'workspace-sidebar',
@@ -15,16 +16,37 @@ import { DividerModule } from 'primeng/divider';
 })
 export class WorkspaceSidebar {
 
+  isUser: boolean = false;
   currentTabIndex: number = 0;
   listMenu: any[] = [];
 
+
+  constructor(
+    private authService: AuthService,
+  ) {
+    this.isUser = this.authService.isUser();
+  }
+
   ngOnInit(): void {
-    this.listMenu = [
-      { type: 'menu', tabIndex: 0, label: 'WORKSPACE_MENU_PROJECT', icon: 'pi pi-th-large' },
-      { type: 'menu', tabIndex: 1, label: 'WORKSPACE_MENU_JOIN', icon: 'pi pi-sign-in' },
-      { type: 'divider', },
-      { type: 'menu', tabIndex: 2, label: 'WORKSPACE_MENU_USERS', icon: 'pi pi-users' },
-    ];
+    this.initMenu();
+  }
+
+  initMenu(): void {
+    if (!this.isUser) {
+      this.listMenu = [
+        { type: 'menu', tabIndex: 0, label: 'WORKSPACE_MENU_PROJECT', icon: 'pi pi-th-large' },
+        { type: 'menu', tabIndex: 1, label: 'WORKSPACE_MENU_JOIN', icon: 'pi pi-sign-in' },
+        { type: 'divider', },
+        { type: 'menu', tabIndex: 2, label: 'WORKSPACE_MENU_USERS', icon: 'pi pi-users' },
+      ];
+    } else {
+      this.listMenu = [
+        { type: 'menu', tabIndex: 0, label: 'WORKSPACE_MENU_PROJECT', icon: 'pi pi-th-large' },
+        { type: 'menu', tabIndex: 1, label: 'WORKSPACE_MENU_JOIN', icon: 'pi pi-sign-in' },
+        { type: 'divider', },
+        { type: 'menu', tabIndex: 3, label: 'FEEDBACK_MENU_SUBMIT', icon: 'pi pi-file-edit' },
+      ];
+    }
   }
 
   onChangeTabIndexEvent = output<number>();

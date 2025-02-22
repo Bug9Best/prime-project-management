eceaaa2d875981209d589c756aef90901af77c1e
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-23 01:59:57 +0700

    done content workspace

--- src/pages/workspace/component/workspace-content/workspace-content.component.ts
+++ src/pages/workspace/component/workspace-content/workspace-content.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { WorkspaceContentProject } from '../../workspace-content-project/workspace-content-project.component';
-import { WorkspaceContentMember } from '../../workspace-content-member/workspace-content-member.component';
 import { WorkspaceContentJoin } from '../../workspace-content-join/workspace-content-join.component';
+import { WorkspaceManageUser } from '../../workspace-manage-user/workspace-manage-user.component';
+import { WorkspaceManageProject } from '../../workspace-manage-project/workspace-manage-project.component';
+import { FormFeedbackSubmit } from '../../../feedback/component/form-feedback-submit/form-feedback-submit.component';
 
 @Component({
   selector: 'workspace-content',
   imports: [
     WorkspaceContentProject,
     WorkspaceContentJoin,
-    WorkspaceContentMember
+    WorkspaceManageProject,
+    WorkspaceManageUser,
+    FormFeedbackSubmit
   ],
   templateUrl: './workspace-content.component.html',
   styleUrl: './workspace-content.component.scss'

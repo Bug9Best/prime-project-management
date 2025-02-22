e7be1d2cb6a68e4c75b4410f53d08e0b54bc6443
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-23 03:32:25 +0700

    remove unuse

--- src/pages/feedback/component/form-feedback-submit/form-feedback-submit.component.ts
+++ src/pages/feedback/component/form-feedback-submit/form-feedback-submit.component.ts
@@ -15,38 +15,4 @@ export class FormFeedbackSubmit {
 
   listForm: InputAppForm[] = listFormFeedbackSubmit;
   formGroup: FormGroup = FormGroupFeedbackSubmit;
-
-
-  // resetForm() {
-  //   this.formFeedbackSubmit.formGroup.reset();
-  // }
-
-  // @ViewChild(FormFeedbackSubmit) formFeedbackSubmit!: FormFeedbackSubmit;
-  // onValidateForm() {
-  //   if (this.formFeedbackSubmit.formGroup.invalid) {
-  //     this.formFeedbackSubmit.formGroup.markAllAsTouched();
-  //     return;
-  //   }
-
-  //   let values = this.formFeedbackSubmit.formGroup.value;
-  //   values.user_id = 1;
-  //   this.onSubmitFeeddback(values);
-  // }
-
-  // @ViewChild(AppDialog) appDialog!: AppDialog;
-  // onSubmitFeeddback(param: any) {
-  //   this.feedbackService
-  //     .createFeedback(param)
-  //     .subscribe({
-  //       next: (response: any) => {
-  //         this.showMessage('success', 'Success', 'Feedback submitted successfully');
-  //         this.appDialog.visible = false;
-  //         this.resetForm();
-  //         this.getFeedbacks();
-  //       },
-  //       error: (error: any) => {
-  //         this.showMessage('error', 'Error', 'Failed to submit feedback');
-  //       }
-  //     });
-  // }
 }

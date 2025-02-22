69ce312a6cb5e84a38673a52f4a6629ec21a2ab1
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-23 03:34:07 +0700

    done feedback list component

--- src/pages/feedback/component/feedback-list/feedback-list.component.ts
+++ src/pages/feedback/component/feedback-list/feedback-list.component.ts
@@ -1,45 +1,127 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppHeader } from '../../../../components/app-header/app-header.component';
 import { AppScrolling } from '../../../../components/app-scrolling/app-scrolling.component';
 import { AppEmpty } from '../../../../components/app-empty/app-empty.component';
 import { FeedbackModel, FeedbackService } from '../../../../services/feedback/feedback.service';
+import { AuthService } from '../../../../services/auth/auth.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { AppDialog } from '../../../../components/app-dialog/app-dialog.component';
+import { FormFeedbackSubmit } from '../form-feedback-submit/form-feedback-submit.component';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'feedback-list',
   imports: [
     CommonModule,
+    TranslateModule,
     AppHeader,
+    AppDialog,
     AppScrolling,
     AppEmpty,
+    FormFeedbackSubmit
   ],
   templateUrl: './feedback-list.component.html',
   styleUrl: './feedback-list.component.scss'
 })
 export class FeedbackList {
 
-  title: string = 'feedback.title.list';
-  subtitle: string = 'feedback.subtitle.list';
-  emptyTitle: string = 'feedback.feedbackEmpty.title';
-  emptyDescription: string = 'feedback.feedbackEmpty.description';
+  currentUserData?: any;
+  isUser: boolean = false;
+
+  title: string = 'feedback.title.admin';
+  subtitle: string = 'feedback.subtitle.admin';
+  emptyTitle: string = 'feedback.feedbackEmptyAdmin.title';
+  emptyDescription: string = 'feedback.feedbackEmptyAdmin.description';
+  labelButton: string = 'feedback.button.submit';
+  labelHeader: string = 'feedback.header.submit';
+  labelSubmit: string = 'app.button.submit';
+  labelFormDescription: string = 'form.feedback.description';
 
   listFeedbacks: FeedbackModel[] = [];
 
   constructor(
+    private authService: AuthService,
+    private messageService: MessageService,
     private feedbackService: FeedbackService
-  ) { }
+  ) {
+    this.isUser = this.authService.isUser();
+    this.currentUserData = this.authService.getUserData();
+    if (this.isUser) {
+      this.title = 'feedback.title.user';
+      this.subtitle = 'feedback.subtitle.user';
+      this.emptyTitle = 'feedback.feedbackEmptyUser.title';
+      this.emptyDescription = 'feedback.feedbackEmptyUser.description';
+    }
+  }
 
   ngOnInit() {
-    this.getFeedbacks();
+    if (this.isUser) {
+      this.getFeedbacks();
+    } else {
+      this.getAllFeedbacks();
+    }
   }
 
   getFeedbacks() {
+    this.feedbackService
+      .getByUser(this.currentUserData.id)
+      .subscribe({
+        next: (response: any) => {
+          this.listFeedbacks = response;
+        },
+      });
+  }
+
+  getAllFeedbacks() {
     this.feedbackService
       .getAll()
       .subscribe({
         next: (response: any) => {
           this.listFeedbacks = response.data;
         },
+      });
+  }
+
+  showMessage(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      key: 'app',
+      severity: severity,
+      summary: summary,
+      detail: detail
+    });
+  }
+
+  resetForm() {
+    this.formFeedbackSubmit.formGroup.reset();
+  }
+
+  @ViewChild(FormFeedbackSubmit) formFeedbackSubmit!: FormFeedbackSubmit;
+  onValidateForm() {
+    if (this.formFeedbackSubmit.formGroup.invalid) {
+      this.formFeedbackSubmit.formGroup.markAllAsTouched();
+      return;
+    }
+
+    let values = this.formFeedbackSubmit.formGroup.value;
+    values.user_id = 1;
+    this.onSubmitFeeddback(values);
+  }
+
+  @ViewChild(AppDialog) appDialog!: AppDialog;
+  onSubmitFeeddback(param: any) {
+    this.feedbackService
+      .createFeedback(param)
+      .subscribe({
+        next: (response: any) => {
+          this.showMessage('success', 'Success', 'Feedback submitted successfully');
+          this.appDialog.visible = false;
+          this.resetForm();
+          this.getFeedbacks();
+        },
+        error: (error: any) => {
+          this.showMessage('error', 'Error', 'Failed to submit feedback');
+        }
       });
   }
 }

1c472578418811cd38b4fe855d10de2d8d1c0eba
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-24 04:21:34 +0700

    update project content component

--- src/pages/project/component/project-content/project-content.component.ts
+++ src/pages/project/component/project-content/project-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { ProjectDashboard } from '../../project-dashboard/project-dashboard.component';
 import { ProjectSprint } from '../../project-sprint/project-sprint.component';
 import { ProjectBacklog } from '../../project-backlog/project-backlog.component';
@@ -8,6 +8,7 @@ import { ProjectSetting } from '../../project-setting/project-setting.component'
 import { ProjectMember } from '../../project-member/project-member.component';
 import { ProjectGantt } from '../../project-gantt/project-gantt.component';
 import { ProjectBoard } from '../../project-board/project-board.component';
+import { ProjectSprintDetail } from '../../project-sprint/project-sprint-detail/project-sprint-detail.component';
 
 @Component({
   selector: 'project-content',
@@ -20,12 +21,32 @@ import { ProjectBoard } from '../../project-board/project-board.component';
     ProjectTask,
     ProjectResource,
     ProjectMember,
-    ProjectSetting
+    ProjectSetting,
+    ProjectSprintDetail
   ],
   templateUrl: './project-content.component.html',
   styleUrl: './project-content.component.scss'
 })
 export class ProjectContent {
 
+  sprintID: string = '';
+  sprintDetailState = signal<boolean>(false);
+
+  taskID: string = '';
+  taskDetailState = signal<boolean>(false);
   currentTabIndex = input<number>(0);
+
+  setSprintState(state: boolean, sprintID?: string) {
+    this.sprintDetailState.set(state);
+    if (sprintID) {
+      this.sprintID = sprintID;
+    }
+  }
+
+  setTaskState(state: boolean, taskID?: string) {
+    this.taskDetailState.set(state);
+    if (taskID) {
+      this.taskID = taskID;
+    }
+  }
 }

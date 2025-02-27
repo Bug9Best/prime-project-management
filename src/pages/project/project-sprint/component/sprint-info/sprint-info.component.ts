73ba03fc3782b5beadc5b61e5a86e60585f8285d
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-28 04:27:46 +0700

    update sprint info component

--- src/pages/project/project-sprint/component/sprint-info/sprint-info.component.ts
+++ src/pages/project/project-sprint/component/sprint-info/sprint-info.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { SprintModel } from '../../../../../services/sprint/sprint.service';
+import { CommonModule } from '@angular/common';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-sprint-info',
-  imports: [],
+  selector: 'sprint-info',
+  imports: [
+    TranslateModule,
+    CommonModule,
+    DividerModule,
+    ButtonModule
+  ],
   templateUrl: './sprint-info.component.html',
   styleUrl: './sprint-info.component.scss'
 })
-export class SprintInfoComponent {
+export class SprintInfo {
 
+  sprintData?: SprintModel = <any>{};
+
+  setSprintData(sprint_data: SprintModel) {
+    this.sprintData = sprint_data;
+  }
 }

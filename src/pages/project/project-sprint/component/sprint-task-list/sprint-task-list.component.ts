851f8cc9caea1ef7eb75fae07acddd221430f4cc
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-02-28 05:01:19 +0700

    prepare creste task list component

---
+++ src/pages/project/project-sprint/component/sprint-task-list/sprint-task-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sprint-task-list',
+  imports: [],
+  templateUrl: './sprint-task-list.component.html',
+  styleUrl: './sprint-task-list.component.scss'
+})
+export class SprintTaskList {
+
+}

d13dea71a75a8fccaa51b928900cf0e0a429b5be
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-03-02 05:54:40 +0700

    partial task list

--- src/pages/project/project-sprint/component/sprint-task-list/sprint-task-list.component.ts
+++ src/pages/project/project-sprint/component/sprint-task-list/sprint-task-list.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { TaskModel, TaskTypeModel } from '../../../../../services/task/task.service';
+import { TagType } from '../../../project-task/component/tag-type/tag-type.component';
+import { TagPriority } from '../../../project-task/component/tag-priority/tag-priority.component';
+import { TagStatus } from '../../../project-task/component/tag-status/tag-status.component';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { AppEmpty } from '../../../../../components/app-empty/app-empty.component';
 
 @Component({
   selector: 'sprint-task-list',
-  imports: [],
+  imports: [
+    AppEmpty,
+    TagPriority,
+    TagStatus,
+    AvatarModule,
+    AvatarGroupModule
+  ],
   templateUrl: './sprint-task-list.component.html',
   styleUrl: './sprint-task-list.component.scss'
 })
 export class SprintTaskList {
 
+  TaskType = TaskTypeModel;
+
+  @Input()
+  listTask?: TaskModel[] = [];
+
+  emptyTitle = 'project.empty.task.title';
+  emptyDescription = 'project.empty.task.description';
 }

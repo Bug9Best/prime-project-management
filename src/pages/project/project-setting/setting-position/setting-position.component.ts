ee6ea4b6b00ea38d3e13cc53c495e6ce7c5430d0
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-03-10 19:50:56 +0700

    create component for position

---
+++ src/pages/project/project-setting/setting-position/setting-position.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'setting-position',
+  imports: [],
+  templateUrl: './setting-position.component.html',
+  styleUrl: './setting-position.component.scss'
+})
+export class SettingPosition {
+
+}

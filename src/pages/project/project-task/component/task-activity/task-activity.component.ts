aa2e532eec13f2d542866ed8d45a1311699fc8ad
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-03-10 10:47:15 +0700

    add type assigns

--- src/pages/project/project-task/component/task-activity/task-activity.component.ts
+++ src/pages/project/project-task/component/task-activity/task-activity.component.ts
@@ -10,7 +10,7 @@ import { TimelineModule } from 'primeng/timeline';
     CommonModule,
     TranslateModule,
     TimelineModule,
-    DividerModule
+    DividerModule,
   ],
   templateUrl: './task-activity.component.html',
   styleUrl: './task-activity.component.scss'
@@ -19,4 +19,24 @@ export class TaskActivity {
 
   @Input()
   listActivity: any = [];
+
+
+  getLogging(item: any) {
+    if (item.action_key == "assigned") {
+      return item.target_user_name;
+    }
+    else if (item.action_key == "sprint_id") {
+      return item.target_sprint_name;
+    } else if (item.action_key == "start_date") {
+      return item.action_value;
+    } else if (item.action_key == "end_date") {
+      return item.action_value;
+    } else if (item.action_key == "estimate_time") {
+      return item.action_value;
+    } else if (item.action_key == "actual_time") {
+      return item.action_value;
+    }
+
+    return 'log.' + item.action_value;
+  }
 }

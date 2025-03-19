27fbd5880a1e71d8d4cd6328b2abb931d7e226ae
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-03-19 12:07:44 +0700

    add get started

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -3,9 +3,7 @@ import { SectionNavigate } from './section-navigate/section-navigate.component';
 import { SectionGetstart } from './section-getstart/section-getstart.component';
 import { SectionFeature } from './section-feature/section-feature.component';
 import { SectionDeveloper } from './section-devloper/section-developer.component';
-import { environment } from '../../environments/environment';
 import { SectionFooter } from './section-footer/section-footer.component';
-import { ProjectService } from '../../services/project/project.service';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
 
@@ -38,17 +36,6 @@ export class HomePage {
           this.router.navigate(['/workspace']);
         }
       });
-  }
-
-  ngOnInit(): void {
-    this.getProjects();
-  }
-
-  projectService = inject(ProjectService);
-  getProjects(): void {
-    this.projectService.getAll().subscribe((projects) => {
-      console.log(projects);
-    });
   }
 
   onScrollToElementEvent(element: string): void {
--- src/pages/home/section-getstart/section-getstart.component.ts
+++ src/pages/home/section-getstart/section-getstart.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../../services/auth/auth.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'section-getstart',
@@ -18,4 +21,22 @@ export class SectionGetstart {
   title = 'home.getStart.title';
   description = 'home.getStart.description';
   buttonLabel = 'home.getStart.buttonLabel';
+
+  auth = inject(AuthService);
+  router = inject(Router);
+  getStarted() {
+    let user = this.auth.getUserData();
+    console.log(user);
+    user ? this.router.navigate(['/workspace']) : this.showMessage('info', 'Info', 'Please sign in to get started');
+  }
+
+  messageService = inject(MessageService);
+  showMessage(severity: string, summary: string, detail: string): void {
+    this.messageService.add({
+      key: 'app',
+      severity: severity,
+      summary: summary,
+      detail: detail
+    });
+  }
 }
